659cb46456e8c3d571385a92381bf365378fa5c8
Marian Veigel <marian.veigel@googlemail.com>
2023-01-28 18:14:58 +0100

    modified script, winner of one game is declared

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+var computerSelection;
+let playerSelection = window.prompt("Rock, Paper or Scissors?").toLowerCase();
+
 function getComputerChoice () {
     var arrayrps = ["rock", "paper", "scissors"];
     let randomnum3 = Math.floor(Math.random()*3);
-    return arrayrps[randomnum3];
+    computerSelection = arrayrps[randomnum3];
+    return computerSelection;
     }
-    console.log(getComputerChoice());
-    
+getComputerChoice();
+console.log(computerSelection);
+
+
+function winner () {
+    if (playerSelection == computerSelection) {
+        window.alert("Draw! Go again!")
+    }
+    else if ((computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "scissors" && playerSelection == "paper") ||
+    (computerSelection == "paper" && playerSelection == "rock")) {
+        window.alert(`You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beat${computerSelection == "scissors" ? "" : "s"} your ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}!`)
+    }
+    else {
+        window.alert(`You win! Your ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beat${playerSelection == "scissors" ? "" : "s"} ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}!`)
+    }
+}
+winner();
+
+//winner prints out you win when anything but rok paper scissors is typed in
+//need to fix that before continuing

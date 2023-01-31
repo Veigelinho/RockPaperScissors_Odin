c15c73efc10305138a6a01378d5454d44134335f
Marian Veigel <marian.veigel@googlemail.com>
2023-01-31 09:12:34 +0100

    modified the game, can play 5 rounds now

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
-var computerSelection;
-let playerSelection = window.prompt("Rock, Paper or Scissors?").toLowerCase();
+var RPS = ["rock", "paper", "scissors"];
+var playerScore = 0;
+var computerScore = 0;
 
 function getComputerChoice () {
-    var arrayrps = ["rock", "paper", "scissors"];
     let randomnum3 = Math.floor(Math.random()*3);
-    computerSelection = arrayrps[randomnum3];
-    return computerSelection;
+    return RPS[randomnum3];
     }
-getComputerChoice();
+const computerSelection = getComputerChoice();
 console.log(computerSelection);
 
+function getPlayerChoice () {
+    return window.prompt(`${RPS[0].charAt(0).toUpperCase() + RPS[0].slice(1) + ", " + RPS[1] + " or " + RPS[2]}?`).toLowerCase();
+}
+const playerSelection = getPlayerChoice();
+console.log(playerSelection);
 
-function winner () {
-    if (playerSelection == computerSelection) {
-        window.alert("Draw! Go again!")
+let bool = true
+
+function playRound () {
+    getComputerChoice();
+    getPlayerChoice();
+    if (!(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors")) {
+        return(`Please choose between ${RPS[0] + ", " + RPS[1] + " or " + RPS[2]}!`)
+    }
+    else if (playerSelection == computerSelection) {
+        return("Draw! Go again!")
     }
     else if ((computerSelection == "rock" && playerSelection == "scissors") ||
     (computerSelection == "scissors" && playerSelection == "paper") ||
     (computerSelection == "paper" && playerSelection == "rock")) {
-        window.alert(`You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beat${computerSelection == "scissors" ? "" : "s"} your ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}!`)
+        bool = false;
+        return(`You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beat${computerSelection == "scissors" ? "" : "s"} your ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}!`)
     }
     else {
-        window.alert(`You win! Your ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beat${playerSelection == "scissors" ? "" : "s"} ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}!`)
+        bool = true;
+        return(`You win! Your ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beat${playerSelection == "scissors" ? "" : "s"} ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}!`)
     }
 }
-winner();
 
-//winner prints out you win when anything but rok paper scissors is typed in
-//need to fix that before continuing
+
+function game () {
+    playerScore = 0;
+    computerScore = 0;
+    var score;
+    for (let i = 0; i<5; i++) {
+        playRound();
+        bool ? playerScore += 1 : computerScore += 1;
+        score = playerScore + ":" + computerScore;
+        console.log(score);
+        console.log(computerScore);
+
+    }
+(playerScore>computerScore) ? console.log("Congrats! You've won the game!") : console.log("You lost! More luck next time!");
+}
+game();

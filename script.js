b3a65459cab1151a61bae05da5ede344626aeb20
Marian Veigel <marian.veigel@googlemail.com>
2023-02-19 17:09:36 +0100

    created ui for game with non functioning restart-button

--- script.js
+++ script.js
@@ -6,51 +6,83 @@ function getComputerChoice () {
     let randomnum3 = Math.floor(Math.random()*3);
     return RPS[randomnum3];
     }
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
 
-function getPlayerChoice () {
-    return window.prompt(`${RPS[0].charAt(0).toUpperCase() + RPS[0].slice(1) + ", " + RPS[1] + " or " + RPS[2]}?`).toLowerCase();
-}
-const playerSelection = getPlayerChoice();
-console.log(playerSelection);
+var playerSelection;
+    
+    const button = document.querySelectorAll("button");
+
+    button.forEach(function(button) {
+        button.addEventListener("click", e => {
+        playerSelection = button.className;
+        // console.log(playerSelection)
+        playRound();
+        })
+    })
+    
+
 
 let bool = true
 
+const divResult = document.createElement("div")
+const divComputerSelection = document.createElement("div")
+const divScore = document.createElement("div")
+const divWinner = document.createElement('div');
+const divRestartButton = document.createElement('button');
+divRestartButton.innerText = "Try Again!";
+
 function playRound () {
-    getComputerChoice();
-    getPlayerChoice();
-    if (!(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors")) {
-        return(`Please choose between ${RPS[0] + ", " + RPS[1] + " or " + RPS[2]}!`)
-    }
-    else if (playerSelection == computerSelection) {
-        return("Draw! Go again!")
-    }
-    else if ((computerSelection == "rock" && playerSelection == "scissors") ||
-    (computerSelection == "scissors" && playerSelection == "paper") ||
-    (computerSelection == "paper" && playerSelection == "rock")) {
-        bool = false;
-        return(`You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beat${computerSelection == "scissors" ? "" : "s"} your ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}!`)
-    }
-    else {
-        bool = true;
-        return(`You win! Your ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beat${playerSelection == "scissors" ? "" : "s"} ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}!`)
+    if((playerScore < 5) && (computerScore < 5)) {    
+        const computerSelection = getComputerChoice();
+            divComputerSelection.innerText = `The computer chose ${computerSelection}!`;
+            
+
+            if (playerSelection == computerSelection) {
+                divResult.innerText = "Draw! Go again!"
+            }
+            else if ((computerSelection == "rock" && playerSelection == "scissors") ||
+            (computerSelection == "scissors" && playerSelection == "paper") ||
+            (computerSelection == "paper" && playerSelection == "rock")) {
+                computerScore += 1;
+                divResult.innerText = `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beat${computerSelection == "scissors" ? "" : "s"} your ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}!`
+            }
+            else {
+                playerScore += 1;
+                divResult.innerText = `You win! Your ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beat${playerSelection == "scissors" ? "" : "s"} ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}!`
+            }    
+                divScore.innerText = `Current score: You ${playerScore} : ${computerScore} computer`;
+    
+        }
+    if ((playerScore == 5) || (computerScore == 5)) {
+        (playerScore>computerScore) ? (divWinner.innerText = "Congrats! You've won the game!") : (divWinner.innerText = "You lost! More luck next time!");
+        document.body.append(divRestartButton)
     }
 }
 
+divRestartButton.addEventListener('click', () => {
+    divRestartButton.remove();
+    
+});
 
-function game () {
-    playerScore = 0;
-    computerScore = 0;
-    var score;
-    for (let i = 0; i<5; i++) {
-        playRound();
-        bool ? playerScore += 1 : computerScore += 1;
-        score = playerScore + ":" + computerScore;
-        console.log(score);
-        console.log(computerScore);
 
-    }
-(playerScore>computerScore) ? console.log("Congrats! You've won the game!") : console.log("You lost! More luck next time!");
-}
-game();
+document.body.append(divResult, divComputerSelection, divScore, divWinner)
+
+
+
+
+
+
+// playerScore = 0;
+// computerScore = 0;
+//     for (let i = 0; i<5; i++) {
+//         playRound();
+//         bool ? playerScore += 1 : computerScore += 1;
+//         score = playerScore + ":" + computerScore;
+//         console.log(score);
+//         console.log(computerScore);
+
+//     }
+// (playerScore>computerScore) ? console.log("Congrats! You've won the game!") : console.log("You lost! More luck next time!");
+
+
+
+// console.log(button);
